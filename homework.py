f65d16febf1a621d322f33cfb3b675d4898b0acf
JimRush <jim.rush@homes.co.nz>
2017-04-26 20:26:38 +1200

    create function headers

--- homework.py
+++ homework.py
@@ -1,7 +1,25 @@
 #Clone this file from the command line, add a comment and push it back up
 
 #Make a function that takes a string (something in quote marks, like "Hello") and prints the reverse out
+def reverseString(string):
+    #reverse the string here
+    #print it out
+    print string
 
-#Make a function that takes two numbers #(either integers/whole numbers or decimals, python doesn't care, it figures out which it's dealing with) and divides the first by the second
 
-#
+#Make a function that takes two numbers #(either integers/whole numbers or decimals,
+#python doesn't care, it figures out which it's dealing with) and divides the first by the second
+
+def divide(a, b):
+    #do something here
+    #print the answer here
+
+#loop through a list of friends and print out each name
+def printFriends(listOfFriends):
+    #do some looping
+        #print a name
+
+
+
+
+reverseString("Hello")
