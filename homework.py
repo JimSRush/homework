9d10c4e2a8564d1bffa557d2221f42f2368ff6c8
JimRush <jim.rush@homes.co.nz>
2017-04-26 20:20:00 +1200

    first commit

---
+++ homework.py
@@ -0,0 +1,7 @@
+#Clone this file from the command line, add a comment and push it back up
+
+#Make a function that takes a string (something in quote marks, like "Hello") and prints the reverse out
+
+#Make a function that takes two numbers #(either integers/whole numbers or decimals, python doesn't care, it figures out which it's dealing with) and divides the first by the second
+
+#
